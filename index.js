d62a33d2bf4ffaa3b1bf04e11bbcb1403fa559c9
NqtT2801 <nguyenanhquan2801@gmail.com>
2023-03-04 20:33:14 +0700

    push

---
+++ index.js
@@ -0,0 +1,23 @@
+const translate = require("translate-google");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 4000;
+
+app.post("/translate", async (req, res) => {
+  const translatedText = await translate(req.body.source, {
+    from: "zh-CN",
+    to: "en",
+  });
+  res.json({result: translatedText});
+});
+
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
